714868dea4d154131041859dd8e175d65be6d4a5
i_am_akash_ <ay157311@gmail.com>
2021-07-15 16:03:36 +0530

    pass by value and pass by reference

---
+++ pass_by_value.go
@@ -0,0 +1,42 @@
+// group A types -> strings, ints, bools, floats, arrays, structs
+// non-pointer wrapper values
+// it's like pass by value
+
+// group B types -> slices, maps, functions
+// pointer wrapper values
+// it's like pass by reference
+package main
+
+import "fmt"
+
+// func updateName(x string) {
+// 	x = "yoshi"
+// }
+
+func updateName(x string) string {
+	x = "wedge"
+	return x
+}
+
+func updateMenu(y map[string]float64) {
+	y["coffee"] = 2.99
+}
+
+func main() {
+	// group A types -> strings, ints, bools, floats, arrays, structs
+	name := "tifa"
+
+	// updateName(name)
+	name = updateName(name)
+
+	fmt.Println(name)
+
+	// group B types -> slices, maps, functions
+	menu := map[string]float64{
+		"pie":       5.95,
+		"ice cream": 3.99,
+	}
+
+	updateMenu(menu)
+	fmt.Println(menu)
+}
