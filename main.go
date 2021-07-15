367f774894c6da0070583690704e6412f03cd4d7
i_am_akash_ <ay157311@gmail.com>
2021-07-15 15:24:25 +0530

    package scope

--- main.go
+++ main.go
@@ -2,6 +2,19 @@ package main
 
 import "fmt"
 
-func main() { // In an application there must only one main function it will act as entrypoint for the aplliction.
-	fmt.Println("Hello, World!!")
+// the scope of this variable is package scope
+var score = 99.5
+
+// cannot use shorthand outside of functions
+// scoreTwo := 50
+
+func main() {
+	// Here we are able to access sayHello() method of greeting.go file
+	// bcoz the package name of both file are same.
+	sayHello("mario")
+	showScore()
+
+	for _, v := range points {
+		fmt.Println(v)
+	}
 }
