e4bc9cf8fb194af9d0e66b32ef2f07195d4628c1
i_am_akash_ <ay157311@gmail.com>
2021-07-16 15:02:34 +0530

    float parser

--- main.go
+++ main.go
@@ -4,54 +4,23 @@ import (
 	"bufio"
 	"fmt"
 	"os"
+	"strconv"
 	"strings"
 )
 
-func getInput(prompt string, r *bufio.Reader) (string, error) {
-	fmt.Print(prompt)
-	input, err := r.ReadString('\n')
-
-	return strings.TrimSpace(input), err
-}
-
-func createBill() bill {
-	reader := bufio.NewReader(os.Stdin)
-
-	name, _ := getInput("Create a new bill name: ", reader)
-
-	b := newBill(name)
-	fmt.Println("Created the bill -", b.name)
-
-	return b
-}
-
-func promptOptions(b bill) {
-	reader := bufio.NewReader(os.Stdin)
-
-	opt, _ := getInput("Choose option (a - add item, s - save bill, t - add tip): ", reader)
-
-	// *************switch case*************
-	switch opt {
-	case "a":
-		name, _ := getInput("Item name: ", reader)
-		price, _ := getInput("Item price: ", reader)
-
-		fmt.Println(name, price)
-	case "t":
-		tip, _ := getInput("Enter tip amount ($): ", reader)
-
-		fmt.Println(tip)
-	case "s":
-		fmt.Println("you chose to save the bill")
-	default:
-		fmt.Println("That was not a valid option...")
-		promptOptions(b)
-	}
-}
-
 func main() {
 
-	mybill := createBill()
-	promptOptions(mybill)
+	reader := bufio.NewReader(os.Stdin)
+	// here we will get float number in string format
+	floatVar, _ := reader.ReadString('\n')
+	// to convert that string into float
+	// strconv.ParseFloat(variableWantToConvert, bit)
+	// this method will retun two value one is convertable float and second one is error
+	// if someone pass the alphabet instead of number so it will give error
+	// bit for you want 64 or 32 bit float
+	floatVar1, err := strconv.ParseFloat(strings.TrimSpace(floatVar), 64)
+	// fmt.Printf("error =%v\n floatVar1=%v", err, floatVar1)
+	fmt.Println("error", err)
+	fmt.Println("value", floatVar1)
 
 }
