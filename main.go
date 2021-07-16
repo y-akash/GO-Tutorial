4b7374b8ef3ef489722ece526c00a522a31b9591
i_am_akash_ <ay157311@gmail.com>
2021-07-16 14:16:06 +0530

    switch case

--- main.go
+++ main.go
@@ -7,32 +7,17 @@ import (
 	"strings"
 )
 
-// func getInput(prompt string, r *bufio.Reader) (string, error) {
-// 	fmt.Print(prompt)
-// 	input, err := r.ReadString('\n')
+func getInput(prompt string, r *bufio.Reader) (string, error) {
+	fmt.Print(prompt)
+	input, err := r.ReadString('\n')
 
-// 	return strings.TrimSpace(input), err
-// }
+	return strings.TrimSpace(input), err
+}
 
 func createBill() bill {
-	// ************************************************
-	// bufio.NewReader(fromWhereYouWantToRead)
-	// bufio.NewReader() => this will create a reader
-	// fromWhereYouWantToRead => it will tell that from where you want to read
-	// here we have mentioned os.Stdin means that we will take it from terminal
 	reader := bufio.NewReader(os.Stdin)
 
-	fmt.Print("Create a new bill name: ")
-
-	// reader.ReadString('\n') this will return two value
-	// first is inputValue and second one will be error
-	// in the ReadString method we only pass character not a string
-	// tells that read till this character
-	// so in this case until you hit enter ie new line till then whichever you write on a terminal will gona read
-	name, error := reader.ReadString('\n')
-	fmt.Println("Error=>", error)
-	name = strings.TrimSpace(name)
-	// name, _ := getInput("Create a new bill name: ", reader)
+	name, _ := getInput("Create a new bill name: ", reader)
 
 	b := newBill(name)
 	fmt.Println("Created the bill -", b.name)
@@ -40,8 +25,33 @@ func createBill() bill {
 	return b
 }
 
+func promptOptions(b bill) {
+	reader := bufio.NewReader(os.Stdin)
+
+	opt, _ := getInput("Choose option (a - add item, s - save bill, t - add tip): ", reader)
+
+	// *************switch case*************
+	switch opt {
+	case "a":
+		name, _ := getInput("Item name: ", reader)
+		price, _ := getInput("Item price: ", reader)
+
+		fmt.Println(name, price)
+	case "t":
+		tip, _ := getInput("Enter tip amount ($): ", reader)
+
+		fmt.Println(tip)
+	case "s":
+		fmt.Println("you chose to save the bill")
+	default:
+		fmt.Println("That was not a valid option...")
+		promptOptions(b)
+	}
+}
+
 func main() {
 
 	mybill := createBill()
-	fmt.Println(mybill)
+	promptOptions(mybill)
+
 }
